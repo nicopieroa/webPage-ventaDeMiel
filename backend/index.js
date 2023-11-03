721d5fc52f3845fb801187fd35bcf4b23fe86ec5
Nicolas Pieroni Aranda <nicolaspieroniaranda@gmail.com>
2023-11-03 16:21:38 -0300

    first time using express

--- backend/index.js
+++ backend/index.js
@@ -1,72 +1,42 @@
-const http = require('node:http')
+const express = require('express')
+const app = express()
 
-const desirePort = process.env.PORT ?? 1234
+const PORT = process.env.PORT ?? 1234
 
-// routing
-const dittoJSON = require('./pokemon-ditto.json')
+app.disable('x-powered-by')
 
-const processRequest = (req, res) => {
-  const { method, url } = req
+// Handle middlewares
+app.use((req, res, next) => {
+  if (req.method !== 'POST') return next()
+  if (req.header['content-type'] !== 'application/json') return next()
 
-  switch (method) {
-    case 'GET':
-      switch (url) {
-        case '/pokemon/ditto':
-          res.setHeader('ContentType', 'application/json; charset=utf-8')
-          return res.end(JSON.stringify(dittoJSON))
-        default:
-          res.statusCode = 404
-          res.setHeader('Content-Type', 'text/html; charset=utf-8')
-          return res.end('<h1>404</h1>')
-      }
+  let body = ''
 
-    case 'POST':
-      switch (url) {
-        case '/pokemon': {
-          let body = ''
+  req.on('data', (chunk) => {
+    body += chunk.toString()
+  })
 
-          req.on('data', (chunk) => {
-            body += chunk.toString()
-          })
+  req.on('end', () => {
+    const data = JSON.parse(body)
+    data.timestamp = Date.now()
+    res.status(201).json(data)
+  })
+})
 
-          req.on('end', () => {
-            const data = JSON.parse(body)
-            // Codigo para corroborar y ver la informacion recibida
-            res.writeHead(201, {
-              'Content-Type': 'application/json; charset=utf-8'
-            })
-            data.timestamp = Date.now()
-            res.end(JSON.stringify(data))
-          })
+// Handle routes
+app.get('/', (req, res) => {
+  res.send('Mi web')
+})
 
-          break
-        }
-        default:
-          res.statusCode = 404
-          res.setHeader('Content-Type', 'text/html; charset=utf-8')
-          return res.end('<h1>404</h1>')
-      }
-  }
-}
-// routing
+app.post('/pokemon', (req, res) => {
+  res.status(201).json(req.body)
+})
 
-// const processRequest = (req, res) => {
-//   if (req.url === '/') {
-//     res.statusCode = 200
-//     res.setHeader('Content-Type', 'text/html; charset=utf-8')
-//     res.end('<h1>Request processed... Home page!!!</h1>')
-//   } else if (req.url === '/contact') {
-//     res.statusCode = 200
-//     res.setHeader('Content-Type', 'text/html; charset=utf-8')
-//     res.end('<h1>Request processed... Contact page!!!</h1>')
-//   } else {
-//     res.statusCode = 400
-//     res.end('<h1>Eror 404, page not found.</h1>')
-//   }
-// }
+// Handle error
+app.use((req, res) => {
+  res.status(404).send('<h1>404</h1>')
+})
 
-const server = http.createServer(processRequest)
-
-server.listen(desirePort, () => {
-  console.log(`server listening on port http://localhost:${desirePort}`)
+app.listen(PORT, () => {
+  console.log(`server listening on por http://localhost:${PORT}`)
 })
